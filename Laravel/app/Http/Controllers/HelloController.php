874e9554ca2039ec9dd8f40747501db79db1172f
Naoya <nanetto777@gmail.com>
2020-03-01 21:28:31 +0900

    Index, Other画面の作成

--- Laravel/app/Http/Controllers/HelloController.php
+++ Laravel/app/Http/Controllers/HelloController.php
@@ -4,21 +4,40 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+global $head, $style, $body, $end;
+$head = '<html><head>';
+$style = <<<EOF
+<style>
+body {font-size:16pt; color:#999;}
+h1 {font-size:100pt; text-align:right; color:#eee; margin;-40px 0px -50px 0px;}
+</style>
+EOF;
+
+$body = '</head><body>';
+$end = '</body></head>';
+
+function tag($tag, $txt){
+  return "<{$tag}>" . $txt . "</{$tag}>";
+}
+
 class HelloController extends Controller
 {
   public function index(){
-    return <<<EOF
+    global $head, $style, $body, $end;
 
-    <!DOCTYPE html>
-    <html lang="en" dir="ltr">
-      <head>
-        <meta charset="utf-8">
-        <title></title>
-      </head>
-      <body>
-        hello word
-      </body>
-    </html>
-    EOF;
+    $html = $head . tag("title",'Hello/Index') . $style . $body .tag('h1', 'Index') . tag('p','this is Index page')
+    . '<a href = "/hello/other">go to Ohter page</a>'
+    . $end;
+    return $html;
+  }
+
+  public function other(){
+    global $head, $style, $body, $end;
+
+    $html = $head . tag('title','Hello/Ohter') . $style . $body
+    . tag('h1','Ohter') . tag('p','this is Other page')
+    . '<a href = "/hello">go to Index page</a>'
+    . $end;
+    return $html;
   }
 }
