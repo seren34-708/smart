40a92613651f33e23cdef1bd77da0888ef06af18
Naoya <nanetto777@gmail.com>
2020-03-02 16:57:03 +0900

    viewを使用

--- Laravel/app/Http/Controllers/HelloController.php
+++ Laravel/app/Http/Controllers/HelloController.php
@@ -3,41 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
-global $head, $style, $body, $end;
-$head = '<html><head>';
-$style = <<<EOF
-<style>
-body {font-size:16pt; color:#999;}
-h1 {font-size:100pt; text-align:right; color:#eee; margin;-40px 0px -50px 0px;}
-</style>
-EOF;
 
-$body = '</head><body>';
-$end = '</body></head>';
-
-function tag($tag, $txt){
-  return "<{$tag}>" . $txt . "</{$tag}>";
-}
-
-class HelloController extends Controller
-{
+class HelloController extends Controller {
   public function index(){
-    global $head, $style, $body, $end;
-
-    $html = $head . tag("title",'Hello/Index') . $style . $body .tag('h1', 'Index') . tag('p','this is Index page')
-    . '<a href = "/hello/other">go to Ohter page</a>'
-    . $end;
-    return $html;
-  }
-
-  public function other(){
-    global $head, $style, $body, $end;
-
-    $html = $head . tag('title','Hello/Ohter') . $style . $body
-    . tag('h1','Ohter') . tag('p','this is Other page')
-    . '<a href = "/hello">go to Index page</a>'
-    . $end;
-    return $html;
+    return view('hello.index');
   }
 }
